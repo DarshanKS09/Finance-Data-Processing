41f34f9d9e655ee9f2926d2088935d041506da98
DarshanKS09 <darshanksgowda09@gmail.com>
2026-04-03 13:22:02 +0530

    all folders created

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("API running");
+});
+
 app.get("/health", (req, res) => {
   res.status(200).json({
     success: true,
--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const connection = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected: ${connection.connection.host}`);
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Mongo Connected");
 };
 
 module.exports = connectDB;
