ea5ebc2e7b10faae846065ea2d4d9c700734f8c0
DarshanKS09 <darshanksgowda09@gmail.com>
2026-04-03 13:03:48 +0530

    folder created

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express  = require("express");
 const app = express();
-
+const PORT = 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API Running");
+  res.send("backend running");
 });
-
+app.listen(PORT,()=>{
+    console.log("Server running on port 5000")
+    console.log("http://localhost:5000")
+})
 module.exports = app;
